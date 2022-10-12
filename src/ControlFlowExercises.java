00679929c1922bc3787f496461236e7f22b13d07
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-10-12 16:44:02 -0500

    Control Flow exercises a.while finished.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,12 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        //1. Loop Basics
+        //While
 
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("The value of i is: " + i);
+            i++;
+        }
     }
 }
