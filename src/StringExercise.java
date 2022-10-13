262d39b95a5d08a1773aec39fbd331a83718b0cf
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-10-13 15:13:50 -0500

    String Basics

---
+++ src/StringExercise.java
@@ -0,0 +1,24 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        //STRING BASICS
+
+        //Problem 1:
+        String theWall = "We dont need no education\nWe dont need no thought control";
+        System.out.println(theWall);
+
+        //Problem 2:
+        String heyListen = "\nCheck \"this\" out!, \"s inside of \"s!";
+        System.out.println(heyListen);
+
+        //Problem 3:
+        String windows = "\nIn windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        //Problem 4:
+        String slashes = "\nI can do backslashes \\, double backslashes \\\\,\nand the amazin triple backslash \\\\\\!";
+        System.out.println(slashes);
+
+
+
+    }
+}
